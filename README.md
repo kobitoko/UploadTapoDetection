47d4977eea2d9c100a7b73ecc7db7f4c9d53931a
RyanSatyabrata <kobitoko@gmail.com>
2025-02-22 17:20:22 -0700

    works nicely, might need to adjust how long it waits and the amount of retries for a new recording to be available on the camera

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
 # UploadTapoDetection
 
-On camera motion detection
-Home assist runs a shell script py:
-something like https://github.com/JurajNyiri/pytapo/blob/main/experiments/DownloadRecordings.py
-download clip file to a folder
-after the py is done that will then rclone folder to remote cloud
+On camera motion detection  
+Home assist runs a shell script py:  
+something like https://github.com/JurajNyiri/pytapo/blob/main/experiments/DownloadRecordings.py  
+download clip file to a folder  
+
+Based on https://github.com/JurajNyiri/pytapo/blob/main/experiments/DownloadRecordings.py  
+needs ffmpeg installed, with bin in path, because e.g. convert.py uses sub process "ffprobe". system_packages.txt is placed in appdaemon/conf/ folder, which appdaemon will scan and install.
+
+local dev:
+`py -m venv ./venv`
+`source ./venv/Scripts/activate`
+`pip install -r requirements.txt`
+
+appdaemon for IDE when not in appdaemon  
+Having trouble with py 3.12, this should work  
+`pip install git+https://github.com/AppDaemon/appdaemon.git`
+
+Would want `thread_duration_warning_threshold: 600` be like 10min(600) rather than default 10s or = 00. This will run long, waiting for new video and downloading it, it shouldnt take longer than 10m unless a big long detection happens?  
+`production_mode: true` when finished setting it up, save some cpu cycles.
+
+Time/timezone is wrong?  
+Set conf/appdaemon.yaml's `time_zone:` entry to desired entry in TZ format:  
+https://en.wikipedia.org/wiki/List_of_tz_database_time_zones  
+or add the docker command `-v /etc/timezone:/etc/timezone:ro`  
+source: https://community.home-assistant.io/t/timezone-setting-in-appdaemon/71756/13
 
 https://appdaemon.readthedocs.io/en/latest/DOCKER_TUTORIAL.html
 
--- apps/upload_tapo_detection.py
+++ apps/upload_tapo_detection.py
@@ -1,21 +1,14 @@
 from pytapo import Tapo
 from pytapo.media_stream.downloader import Downloader
-import sys
 import os
 import stat
 import shutil
-import time
 from datetime import datetime, timedelta
 from dataclasses import dataclass
 from appdaemon.plugins.hass import hassapi as hass
 
 # Taken from https://github.com/JurajNyiri/pytapo/blob/main/experiments/DownloadRecordings.py
-# mandatory:
-# ffmpeg installed, with bin in path, because e.g. convert.py uses sub process "ffprobe"
-# also assumes you've set up rclone already
-# py -m venv ./venv
-# source ./venv/Scripts/activate
-# pip install -r requirements.txt
+# needs ffmpeg installed, with bin in path, because e.g. convert.py uses sub process "ffprobe"
 class UploadTapoDetection(hass.Hass):
 
     @dataclass
@@ -26,23 +19,25 @@ class UploadTapoDetection(hass.Hass):
         isValid: bool = False
 
     def initialize(self):
-        self.outputDir = self.args["output"] #os.environ.get("UTAPOD_OUTPUT")# directory path where videos will be saved
-        self.host = self.args["host"] #os.environ.get("UTAPOD_HOST")  # change to camera IP
-        self.destination = self.args["destination"]
-        self.passwordCloud = self.args["password_cloud"] #os.environ.get("UTAPOD_PASSWORD_CLOUD")  # set to your cloud password
-        self.entityId = self.args["entity_id"]
-        self.tapo = 0 # initialize, we'll get this as needed
-        self.date = "" # initialize, we'll set this when we want to upload
-        self.startDetectionTime = datetime.now() # detection time to make sure the latest video is gotten.
+        self.outputDir = self.args["output"] # directory path where the video will be downloaded to and saved, include ending "/"
+        self.host = self.args["host"] # change to camera IP
+        self.destination = self.args["destination"] # directory path where videos will be copied to, after downloading them, include ending "/"
+        self.passwordCloud = self.args["password_cloud"] # set to your cloud password
+        self.entityId = self.args["entity_id"] # entity id of the sensor from home assistant that will trigger this script
+        self.tapo = 0 # initialize pytapo, we'll get this as needed
+        self.date = "" # initialize date, we'll set this when we want to download
+        self.startDetectionTime = datetime.now() # detection time to make sure a recent video is gotten.
         # optional
         self.window_size = 100 #os.environ.get("WINDOW_SIZE")  # set to prefferred window size, affects download speed and stability, recommended: 50, default is 200
-        #self.listen_event(self.runActionTask, "state_changed")
         self.listen_event(self.runActionTask)
         self.log("Initialized!")
+        #TEST
+        #self.startDetectionTime = datetime(year=2025, month=2, day=17, hour=20, minute=22, second=17) #TEST
+        #self.execute() #TEST
 
     def runActionTask(self, event_name, data, cb_args):
         '''
-        # See all the events and their data from home assistant.
+        # Uncomment to see all the events and their data from home assistant.
         self.log(event_name)
         self.log("data dict:")
         for k,v in data.items():
@@ -60,65 +55,80 @@ class UploadTapoDetection(hass.Hass):
         if oldState == "off" and newState == "on":
             self.startDetectionTime = datetime.now()
         elif oldState == "on" and newState == "off":
-            try:
-                # perhaps create a scheduled task that would run every couple seconds like 5, but only for like 20 times or something.
-                # and modify that if the local file already exists with the latest name, it skips 
-                self.create_task(self.execute())
-            except:
-                self.log("Something went really wrong. Exiting now.")
+            self.execute()
 
-    async def execute(self):
+    def execute(self):
+        #self.date = "20250217"# this is just a test
         self.date = datetime.now().strftime("%Y%m%d") # date to download recordings for in format YYYYMMDD
-        #self.date = "20250217"#TODO this is just test, uncomment above
         self.log("Connecting to camera...")
         self.tapo = Tapo(self.host, "admin", self.passwordCloud, self.passwordCloud)
-        timeTaken = datetime.now()
-        DownloadedFile = await self.DownloadAsync()
-        if not DownloadedFile.isValid:
+        self.create_task(self.DownloadAsync(), callback=self.MoveDownload)
+    
+    def MoveDownload(self, kwargs):
+        #self.log(str(kwargs)) # what is in kwargs?
+        DownloadedFile = kwargs["result"]
+        if ("result" not in kwargs) or (not DownloadedFile.isValid):
             self.log("Downloaded file was invalid!")
             return
-        self.log("Download time taken: " + str(datetime.now() - timeTaken))
         newFilePath = "{}{}".format(self.destination, DownloadedFile.fileName)
+        if not os.path.isfile("{}{}".format(self.outputDir, DownloadedFile.fileName)):
+            self.log("File '{}' not found! Wasn't in the outputDir '{}'. Cannot move it to destination.".format(DownloadedFile.fileName, self.outputDir))
+        self.log("Moving downloaded file to {}.".format(self.destination))
         shutil.move("{}{}".format(self.outputDir, DownloadedFile.fileName), newFilePath)
+        # permissions might need to change if folder is synced to the cloud with a different software.
         os.chmod(newFilePath, stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IWGRP | stat.S_IROTH | stat.S_IWOTH)
-        self.log("Finished!")
+        self.log("Finished!\n")
 
     async def GetFileInfo(self):
         self.log("Getting recordings...")
         recordings = self.tapo.getRecordings(self.date)
         startEnd = (0,0)
-        #for retry in range(6):
+        fileName = ""
+        # retry up to 30 seconds after trigger ended.
+        retries = range(30)
+        # 30s before detection time, as some cctv make the clips several seconds before it.
+        startTimeVideo = self.startDetectionTime - timedelta(seconds=30)
+        for retry in retries:
             # name all videos of that date, and only download latest
-        # give some time for the video to be made on the device, 5s is not long enough, and 10s is limit, so appdaemon will kill it...
-        #time.sleep(5)
-        for recording in recordings:
-            for key in recording:
-                start = datetime.fromtimestamp(recording[key]["startTime"]).strftime('%Y-%m-%d %H:%M:%S')
-                end = datetime.fromtimestamp(recording[key]["endTime"]).strftime('%Y-%m-%d %H:%M:%S')
-                self.log("Video file key: " + key + " start " + start + " end: " + end)
-                # 30s before detection time, as some cctv make the clips several seconds before it.
-                #if datetime.fromtimestamp(recording[key]["startTime"]) < self.startDetectionTime - timedelta(seconds=30):
-                    # dont even bother with older recordings
-                    #continue
-                if(startEnd[0] < recording[key]["startTime"]):
-                    startEnd = (recording[key]["startTime"], recording[key]["endTime"])
-            # if retry == 8:
-            #     self.log("Never found a file earlier than {}... exiting.".format(datetime.fromtimestamp(self.startDetectionTime).strftime('%Y-%m-%d %H:%M:%S')))
-            #     return self.FileInfo()
-            # else:
-            #     self.log("No file found earlier than {}, sleep for 1 second".format(datetime.fromtimestamp(self.startDetectionTime).strftime('%Y-%m-%d %H:%M:%S')))
-            #     time.sleep(1)
-        # Name format: "YYYY.mm.dd_hh.mm.ss-hh.mm.ss.mp4"
-        fileName = "{}-{}.mp4".format(datetime.fromtimestamp(startEnd[0]).strftime('%Y.%m.%d_%H.%M.%S'), datetime.fromtimestamp(startEnd[1]).strftime('%H.%M.%S'))
+            for recording in recordings:
+                for key in recording:
+                    start = datetime.fromtimestamp(recording[key]["startTime"]).strftime('%Y-%m-%d %H:%M:%S')
+                    end = datetime.fromtimestamp(recording[key]["endTime"]).strftime('%Y-%m-%d %H:%M:%S')
+                    self.log("Video file key: " + key + " start " + start + " end: " + end)
+                    if datetime.fromtimestamp(recording[key]["startTime"]) < startTimeVideo:
+                        # dont even bother with older recordings
+                        self.log("Skipping: its older than {}".format(startTimeVideo))
+                        continue
+                    if(startEnd[0] < recording[key]["startTime"]):
+                        startTime = recording[key]["startTime"]
+                        endTime = recording[key]["endTime"]
+                        # Name format: "YYYY.mm.dd_hh.mm.ss-hh.mm.ss.mp4"
+                        fileNameCandidate = "{}-{}.mp4".format(datetime.fromtimestamp(startTime).strftime('%Y.%m.%d_%H.%M.%S'), datetime.fromtimestamp(endTime).strftime('%H.%M.%S'))
+                        # make sure we don't already have that file
+                        if not os.path.isfile("{}{}".format(self.destination, fileNameCandidate)):
+                            startEnd = (startTime, endTime)
+                            fileName = fileNameCandidate
+                        else:
+                            self.log("Skipping: Found existing file '{}{}'".format(self.destination, fileName))
+            if fileName != "":
+                # Found a file to download.
+                break
+            if retry == retries[-1]:
+                self.log("Never found a file later than {} after 30 retries, Exiting.".format(startTimeVideo.strftime('%Y-%m-%d %H:%M:%S')))
+                return self.FileInfo()
+            else:
+                self.log("No file found later than {}, sleep for 1 second. retry:{}".format(startTimeVideo.strftime('%Y-%m-%d %H:%M:%S'), retry))
+                await self.sleep(1)
         return self.FileInfo(fileName, startEnd[0], startEnd[1], startEnd[0] != 0 and startEnd[1] != 0)
 
     async def DownloadAsync(self):
+        timeTaken = datetime.now()
         FileToDownload = await self.GetFileInfo()
         if not FileToDownload.isValid:
             return FileToDownload
-        #return#test
         timeCorrection = self.tapo.getTimeCorrection()
         self.log("Download recording {}".format(FileToDownload.fileName))
+        #return#test
 
         downloader = Downloader(
             self.tapo,
@@ -143,5 +153,5 @@ class UploadTapoDetection(hass.Hass):
             else:
                 statusString += "..."
             self.log(statusString + (" " * 10) + "\r")
-        self.log("\n")
+        self.log("Download time taken: " + str(datetime.now() - timeTaken))
         return FileToDownload
